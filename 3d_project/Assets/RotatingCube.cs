92868db3b36707749b360a69566a5633892dcefc
Arvydas <azegaspa@gmail.com>
2024-12-28 12:56:46 +0200

    public variable editRotation, rotating

--- 3d_project/Assets/RotatingCube.cs
+++ 3d_project/Assets/RotatingCube.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class RotatingCube : MonoBehaviour
 {
+    public Vector3 editRotation;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Hello World!");
-        transform.Rotate(0, 45, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(editRotation);
     }
 }
